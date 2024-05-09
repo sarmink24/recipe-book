425ac54c074c97160ebde5592530cf8f8d1722c9
Sarmin <iamsarminkhatun@gmail.com>
2024-05-09 17:28:06 +0530

    search

--- src/components/form/RecipeForm.js
+++ src/components/form/RecipeForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./RecipeForm.css"; // Import the CSS file for styling
+import { Modal } from "react-bootstrap";
 
 const RecipeForm = ({ onSubmit }) => {
   const [name, setName] = useState("");
@@ -17,33 +18,40 @@ const RecipeForm = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="recipe-form">
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Preparation Time"
-        value={preparationTime}
-        onChange={(e) => setPreparationTime(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Servings"
-        value={servings}
-        onChange={(e) => setServings(e.target.value)}
-      />
-      <button type="submit">Add Recipe</button>
-    </form>
+    <Modal>
+      <Modal.Header></Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleSubmit} className="recipe-form">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Preparation Time"
+            value={preparationTime}
+            onChange={(e) => setPreparationTime(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Servings"
+            value={servings}
+            onChange={(e) => setServings(e.target.value)}
+          />
+        </form>
+      </Modal.Body>
+      <Modal.Footer>
+        <button type="submit">Add Recipe</button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import "./SearchBar.css"; // Import the CSS file for styling
+import { CgAdd } from "react-icons/cg";
+import { FaSearch } from "react-icons/fa";
+import RecipeForm from "../form/RecipeForm";
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -11,15 +23,26 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="search-bar">
-      <input
-        type="text"
-        placeholder="Search Recipes"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button type="submit">Search</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="search-bar">
+        <input
+          type="text"
+          placeholder="Search Recipes"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+      <div className="add">
+        <button onClick={openModal}>
+          <CgAdd className="add-icon" />
+          Add
+        </button>
+        {isModalOpen && <RecipeForm onClose={closeModal} />}
+      </div>
+    </>
   );
 };
 
