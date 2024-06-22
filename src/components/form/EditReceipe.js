2f7f3cd39016e2b2c94da00be2bb9e88d25e9397
Sarmin <iamsarminkhatun@gmail.com>
2024-06-22 23:01:37 +0530

    ui changes

--- src/components/form/EditReceipe.js
+++ src/components/form/EditReceipe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./EditRecipe.css"; // Import the CSS file for styling
+import "./EditRecipe.css"; 
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
--- src/components/lists/RecipeList.js
+++ src/components/lists/RecipeList.js
@@ -1,17 +1,28 @@
+
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./RecipeList.css";
+import { BiDetail } from "react-icons/bi";
+import { CiEdit } from "react-icons/ci";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Modal from "../Modal";
+import EditRecipe from "../form/EditReceipe";
 
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
   const [items, setItems] = useState([]);
   const [expandedId, setExpandedId] = useState(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
 
-  const alertMessage = location.state && location.state.alertMessage;
-  const searchResults = location.state && location.state.searchResults;
+  const alertMessage = location.state?.alertMessage;
+  const searchResults = location.state?.searchResults;
 
   const navigateToRecipe = (recipe) => {
     navigate(`/desserts/${recipe.id}`, { state: { recipe } });
@@ -38,6 +49,58 @@ const RecipeList = () => {
     }
   }, [searchResults, recipes]);
 
+  const handleDeleteClick = (recipe) => {
+    toast(
+      ({ closeToast }) => (
+        <div>
+          <p>Are you sure you want to delete this recipe?</p>
+          <button
+            onClick={() => {
+              handleDeleteConfirmation(recipe);
+              closeToast();
+            }}
+            className="toast-button"
+          >
+            Yes
+          </button>
+          <button onClick={closeToast} className="toast-button">
+            No
+          </button>
+        </div>
+      ),
+      {
+        closeOnClick: false,
+        draggable: false,
+      }
+    );
+  };
+
+  const handleDeleteConfirmation = async (recipe) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/desserts/${recipe.id}`,
+        {
+          method: "DELETE",
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error("Failed to delete the recipe");
+      }
+  
+      // Update items state by filtering out the deleted recipe
+      setItems(prevItems => prevItems.filter(item => item.id !== recipe.id));
+  
+      toast.success("Recipe deleted successfully!", {
+        onClose: () => navigate("/desserts"),
+      });
+    } catch (error) {
+      console.error("Error deleting recipe:", error);
+      toast.error("Error deleting recipe. Please try again.");
+    }
+  };
+  
+
   const toggleDescription = (id) => {
     setExpandedId(id === expandedId ? null : id);
   };
@@ -46,22 +109,29 @@ const RecipeList = () => {
     navigate("/desserts", { state: { alertMessage: null } });
   };
 
+  const handleEditClick = (recipe) => {
+    setSelectedRecipe(recipe);
+    setIsEditModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsEditModalOpen(false);
+    setIsDeleteModalOpen(false);
+    setSelectedRecipe(null);
+  };
+
   return (
     <div className="RecipeList">
       {alertMessage ? (
         <>
           <p>{alertMessage}</p>
-          <p onClick={handleGoBack}>Go back!</p>
+          <p onClick={handleGoBack} style={{ cursor: 'pointer' }}>Go back!</p>
         </>
       ) : (
         <>
           {items.map((recipe) => (
-            <div
-              key={recipe.id}
-              className="recipe"
-              onClick={() => navigateToRecipe(recipe)}
-            >
-              <div className="recipe-photo">
+            <div key={recipe.id} className="recipe">
+              <div className="recipe-photo" onClick={() => navigateToRecipe(recipe)}>
                 <img src={recipe.imageUrl} alt={recipe.name} />
               </div>
               <div className="recipe-details">
@@ -80,34 +150,31 @@ const RecipeList = () => {
                   </button>
                 </p>
               </div>
+              <div className="actions-button">
+                <button onClick={() => navigateToRecipe(recipe)} className="edit-b">
+                  <BiDetail className="detail-icon" />
+                  Details
+                </button>
+                <button onClick={() => handleEditClick(recipe)} className="edit-b">
+                  <CiEdit className="edit-i" />
+                  Edit
+                </button>
+                <button onClick={() => handleDeleteClick(recipe)} className="delete-b">
+                  <RiDeleteBin6Line className="delete-i" />
+                  Delete
+                </button>
+              </div>
             </div>
           ))}
-          {searchResults ? <p onClick={handleGoBack}>Go back!</p> : <p></p>}
+          {searchResults && <p onClick={handleGoBack} style={{ cursor: 'pointer' }}>Go back!</p>}
+
+          <Modal isOpen={isEditModalOpen} onClose={closeModal}>
+        {selectedRecipe && <EditRecipe recipe={selectedRecipe} />}
+      </Modal>
+
+      <ToastContainer />
         </>
       )}
-      {/* {items.map((recipe) => (
-        <div key={recipe.id} className="recipe">
-          <div className="recipe-photo">
-            <img src={recipe.imageUrl} alt={recipe.name} />
-          </div>
-          <div className="recipe-details">
-            <div className="title-price">
-              <h3 className="price">
-                <mark>${recipe.price}</mark>
-              </h3>
-              <h3 className="title">{recipe.name}</h3>
-            </div>
-            <p>
-              {expandedId === recipe.id
-                ? recipe.description
-                : `${recipe.description.slice(0, 40)}... `}
-              <button onClick={() => toggleDescription(recipe.id)}>
-                {expandedId === recipe.id ? "Read Less" : "Read More"}
-              </button>
-            </p>
-          </div>
-        </div>
-      ))} */}
     </div>
   );
 };
